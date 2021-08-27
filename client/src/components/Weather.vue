21784e4eb3682e447165453d87603891f56abf7c
Lee Murray <mrleemurray@gmail.com>
2021-08-27 12:27:33 +0100

    add weather formatting

--- client/src/components/Weather.vue
+++ client/src/components/Weather.vue
@@ -8,24 +8,58 @@
                 :key="weather.dt"
                 class="weather__image"
                 />
-                
             </transition>
         </div>
         <div class="weather__details">
-            <h1
+            <p
                 class="weather__temperature"
-                v-text="temperature()"
-            />
-            <h2
-                class="weather__humidity"
-                v-text="weather.metric.main.humidity"
-            />
+            >
+                <span v-text="formatTemperature(weather.imperial.main.temp, 'imperial')"/>
+                <span class="weather__divider">/</span>
+                <span v-text="formatTemperature(weather.metric.main.temp, 'metric')"/>
+            </p>
+            <div :class="isTempFeelDifferent(weather.imperial.main)">
+            <p class="weather__description">Feels like</p>
+            <p
+                class="weather__temperature weather__feel"
+            >
+                <span v-text="formatTemperature(weather.imperial.main.feels_like, 'imperial')"/>
+                <span class="weather__divider">/</span>
+                <span v-text="formatTemperature(weather.metric.main.feels_like, 'metric')"/>
+            </p>
+            </div>
+            <div class="weather__extra">
+                <div class="weather__section">
+                <p class="weather__description weather__smaller">Humidity</p>
+                <p
+                    class="weather__humidity"
+                    v-text="formatHumidity(weather.metric.main.humidity)"
+                />
+                </div>
+                <div class="weather__section">
+                <p class="weather__description weather__smaller">Wind</p>
+                <p
+                    class="weather__humidity"
+                    v-text="formatWind(weather.imperial.wind.speed)"
+                />
+                </div>
+                <div class="weather__section">
+                <p class="weather__description weather__smaller">Gusts</p>
+                <p
+                    class="weather__humidity"
+                    v-text="formatWind(weather.imperial.wind.gust)"
+                />
+                </div>
+            </div>
         </div>
   </div>
 </template>
 
 <script>
 var weather = require('openweather-apis')
+
+const METRIC = 'metric'
+const IMPERIAL = 'imperial'
 
 export default {
   name: 'Weather',
@@ -78,12 +112,12 @@ export default {
     },
     getLatestWeather() {
       var self = this;
-      weather.setUnits('metric')
+      weather.setUnits(METRIC)
       weather.getAllWeather((err, JSONObj) => {
         self.weather.metric = JSONObj
         self.weather.image = `https://source.unsplash.com/720x720/?${self.weather.metric.weather[0].description.replace(/ /g, ",")}`
       })
-      weather.setUnits('imperial')
+      weather.setUnits(IMPERIAL)
       weather.getAllWeather((err, JSONObj) => {
         self.weather.imperial = JSONObj
       })
@@ -91,7 +125,27 @@ export default {
     },
     temperature(){
         return `${Math.round(this.weather.metric.main.temp)}°C / ${Math.round(this.weather.imperial.main.temp)}°F`
-      }
+    },
+    formatTemperature(temp, unit) {
+        console.info(unit)
+        if (unit === 'metric'){
+            return `${Math.round(temp)}°C`
+        } else {
+            return `${Math.round(temp)}°F`
+        }
+    },
+    formatHumidity(humidity) {
+        return `${humidity}%`
+    },
+    formatWind(speed) {
+        return `${Math.round(speed)}mph`
+    },
+    isTempFeelDifferent(temperature){
+        if (Math.round(temperature.temp) == Math.round(temperature.feels_like)){
+            return 'weather--opacity-0'
+        }
+        return ''
+    }
   },
 
   watch: {}
